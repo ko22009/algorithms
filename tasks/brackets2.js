f045871963879e2ee2a5cd19ff31a1b148d45b9a
Ruslan Kutliakhmetov <ruslan.kutliakhmetov@1win.pro>
2026-02-03 12:55:35 +0500

    fix brackets algorithm

--- tasks/brackets2.js
+++ tasks/brackets2.js
@@ -1,23 +1,20 @@
-const openedBrackets = ["{", "[", "("];
-const closedBrackets = ["}", "]", ")"];
+const openedBrackets = {
+  "{": "}",
+  "[": "]",
+  "(": ")",
+};
 
 function brackets(str) {
   const stack = [];
   const chars = str.split("");
   for (let char of chars) {
-    if (openedBrackets.includes(char)) {
+    if (char in openedBrackets) {
       stack.push(char);
-    } else if (closedBrackets.includes(char)) {
-      if (stack.length === 0) {
-        return false;
-      }
-      const current = stack.pop();
-      const i = openedBrackets.findIndex((el) => el === current);
-      if (closedBrackets[i] !== char) {
-        return false;
-      }
     } else {
-      return false;
+      const current = stack.pop();
+      if (openedBrackets[current] !== char) {
+        return false;
+      }
     }
   }
 
